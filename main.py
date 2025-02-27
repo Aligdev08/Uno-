c812993d2b6c7d80dd0f1c832f73012723a8f426
Aligdev08 <19AliGhali@georgespencer.org.uk>
2025-02-27 00:02:37 +0000

    "working state"

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 import random
 from helper import make_ordinal
+from abc import abstractmethod
 
 colours = ["blue", "red", "green", "yellow"]
+
+
+class Player:
+    def __init__(self, name: str, hand: list["Card"]):
+        self.name = name
+        self.hand = hand
+
+
+class Piles:
+    def __init__(self, deck: list["Card"]):
+        random.shuffle(deck)
+        self.deck = deck
+        self.discard = []
+        self.forwards = True
 
 
 class Card:
@@ -21,23 +36,18 @@ class Card:
         colour: str = getattr(self, "colour", None)
         value: int = getattr(self, "_value", None)
 
-        self.name = f"{colour.capitalize()}{f' {value} ' if value else ' '}{class_name}" if colour else class_name
+        self.name = (
+            f"{colour.capitalize()}{f' {value} ' if value else ' '}{class_name}"
+            if colour
+            else class_name
+        )
+
+    @abstractmethod
+    def play(self, piles: Piles, player: Player, hand_index: int):
+        pass
 
     def __repr__(self):
         return self.name
-
-
-class Player:
-    def __init__(self, name: str, hand: list[Card]):
-        self.name = name
-        self.hand = hand
-
-
-class Piles:
-    def __init__(self, deck: list[Card]):
-        random.shuffle(deck)
-        self.deck = deck
-        self.discard = []
 
 
 class ColouredCard(Card):
@@ -60,11 +70,18 @@ class StandardCard(ColouredCard):
     def play(self, piles: Piles, player: Player, hand_index: int):
         piles.discard.append(self)
         player.hand.pop(hand_index)
+        print(f"{player.name} played {self}!")
 
 
 class ReverseCard(ColouredCard):
     def __init__(self, colour: str):
         super().__init__(colour)
+
+    def play(self, piles: Piles, player: Player, hand_index: int):
+        piles.forwards = not piles.forwards
+        piles.discard.append(self)
+        player.hand.pop(hand_index)
+        print(f"{player.name} played {self}!")
 
 
 class SkipCard(ColouredCard):
@@ -96,7 +113,9 @@ def generate_piles() -> Piles:
                 default_deck.append(DrawTwoCard(colour))
                 default_deck.append(ReverseCard(colour))
                 default_deck.append(SkipCard(colour))
-            default_deck.append(StandardCard(colour, (i / 2).__trunc__()))  # add the right values using i
+            default_deck.append(
+                StandardCard(colour, (i / 2).__trunc__())
+            )  # add the right values using i
             #  0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9
 
     return Piles(default_deck)
@@ -127,6 +146,20 @@ def request_player_name(index: int) -> str:
     return player_name_
 
 
+def request_card_number(max_num: int) -> int:
+    response = input(f"Input the hand number of the card that you would like to play? ")
+    try:
+        card_number_ = int(response)
+    except ValueError:
+        print("Please input a valid number.")
+        return request_card_number()
+    if card_number_ > max_num or card_number_ < 1:
+        print("Please input a number that's in your hand.")
+        return request_card_number(max_num)
+
+    return card_number_
+
+
 def generate_hand(deck: list[Card]):
     hand = []
     for _ in range(1, 7):
@@ -136,27 +169,47 @@ def generate_hand(deck: list[Card]):
     return hand
 
 
+def print_hand(player: Player):
+    print(
+        f"-------------------------- Hand of '{player.name}' --------------------------"
+    )
+    for index, card in enumerate(player.hand, start=1):
+        print(f"{index}. {card}")
+    print("\n")
+
+
 def print_hands(players: list[Player]):
     for player in players:
-        print(f"-------------------------- Hand of \'{player.name}\' --------------------------\n"
-              f"- {f'\n- '.join(str(card) for card in player.hand)}"
-              )
+        print_hand(player)
 
 
 player_count = request_player_count()
 
 piles = generate_piles()
 
-players = []
+players: list[Player] = []
 for i in range(0, player_count):
     player_name = request_player_name(i)
-    player = Player(player_name,
-                    generate_hand(piles.deck)
-                    )
+    player = Player(player_name, generate_hand(piles.deck))
 
-    players.append(
-        player
-    )
+    players.append(player)
 
+done = False
 print_hands(players)
+active_player = 0
 
+while not done:
+    player = players[active_player]
+    print(
+        f"\n--------------------------------------- {player.name}'s turn. ---------------------------------------"
+    )
+    print_hand(player)
+    card_number = request_card_number(len(player.hand))
+    player.hand[card_number - 1].play(piles, player, card_number - 1)
+
+    if piles.forwards:
+        active_player += 1
+    else:
+        active_player -= 1
+        if active_player < 0 < len(players):
+            active_player = len(players) - 1
